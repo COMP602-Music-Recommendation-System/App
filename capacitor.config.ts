92451cfda8c2515e36183caf526eb1f99c61ae9e
Ricky Tsai <ricky@rtnztech.com>
2025-04-18 21:00:19 +1200

    Change app Id

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.rtnztech.music',
   appName: 'Music Recommendation',
   webDir: 'www'
 };
